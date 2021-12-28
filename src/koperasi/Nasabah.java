b1bf40ab42f042bfb13894cb1bc806736082d639
Kurniawan Dwi Yuliyanto <83492070+KurniawanDwiYuliyanto2057051010@users.noreply.github.com>
2021-12-27 16:27:15 -0800

    Update Nasabah.java

--- src/koperasi/Nasabah.java
+++ src/koperasi/Nasabah.java
@@ -28,3 +28,57 @@ public abstract class Nasabah {
     }
     
 
+    public int getID() {
+        return ID.get();
+    }
+
+    public void setID(int ID) {
+        this.ID.set(ID);
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public String getAddress() {
+        return address.get();
+    }
+
+    public void setAddress(String address) {
+        this.address.set(address);
+    }
+
+    public int getNumAccount() {
+        return numAccount.get();
+    }
+
+    public ArrayList<Rekening> getRekening() {
+        return rekening;
+    }
+
+    public void setRekening(ArrayList<Rekening> rekening) {
+        this.rekening = rekening;
+    }
+    
+    public IntegerProperty IDProperty(){
+        return this.ID;
+    }
+    
+    public StringProperty nameProperty(){
+        return this.name;
+    }
+    
+    public StringProperty addressProperty(){
+        return this.address;
+    }
+    
+    public IntegerProperty numAccountProperty(){
+        return this.numAccount;
+    }
+    
+    public abstract void print();
+}
