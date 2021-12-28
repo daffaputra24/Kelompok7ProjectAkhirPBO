bca18e942380a9b84a282febd95372f80826af87
Kurniawan Dwi Yuliyanto <83492070+KurniawanDwiYuliyanto2057051010@users.noreply.github.com>
2021-12-27 16:29:11 -0800

    Update Individu.java

--- src/koperasi/Individu.java
+++ src/koperasi/Individu.java
@@ -19,32 +19,4 @@ public class Individu extends Nasabah{
         this.npwp = new SimpleLongProperty(npwp);
     }
 
-    public long getNik() {
-        return nik.get();
-    }
 
-    public void setNik(long nik) {
-        this.nik.set(nik);
-    }
-
-    public long getNpwp() {
-        return npwp.get();
-    }
-
-    public void setBirhtdate(long npwp) {
-        this.npwp.set(npwp);
-    }
-    
-    public LongProperty nikProperty(){
-        return this.nik;
-    }
-    
-    public LongProperty npwpProperty(){
-        return this.npwp;
-    }
-
-    @Override
-    public void print() {
-        
-    }
-}
